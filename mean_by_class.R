5392b8fa08794bf39f04e5c9e14b1df7d7ab72b2
Jack Wong <waiyujack@gmail.com>
2020-01-11 22:38:50 +0000

    First commit

---
+++ mean_by_class.R
@@ -0,0 +1,23 @@
+library(plyr)
+
+mean_by_class <- function(dataset,
+                 factors,
+                 response) {
+  
+  #INPUT: dataset
+  #       classes
+  #       response
+  #OUTPUT
+  
+  output <- plyr::ddply(.data = dataset,
+                       .variables = factors,
+                       .fun = summarise,
+                           mean_response = mean(get(reponse),na.rm = T),
+                           no_obs = length(get(reponse))
+  )
+  
+  
+              
+  
+  
+}
